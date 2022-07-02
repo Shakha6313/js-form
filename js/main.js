2d898423d3ae682f406afa5ac0eaaa4b079f2a3a
Shakha6313 <shaxapilot@gmail.com>
2022-07-02 23:16:00 +0500

    123

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var name = prompt('What is your name?');
+var surname = prompt('What is your surname?');
+var age = prompt('How old are you?');
+var location = prompt('Where do you live?')
+var study = prompt('Do you study?');
+var work = ('Do you work?');
+var married = prompt('Are you married?');
+var hobby = prompt('What is your hobby?');
+var dream = prompt('What is your dream?');
+var cash = prompt('How much do you earn monthly?')
+
+ console.log(`Name:${name}\nSurname:${surname}\nAge:${age}\nLocation:${location}\nStudy:${study}\nWork:${work}\nMarried:${married}\nHobby:${hobby}\nDream:${dream}\nCash:${cash}`);
